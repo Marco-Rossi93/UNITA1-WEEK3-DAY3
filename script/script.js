d3768e26a8bef90af779425ba60bfe0f1d4af705
Marco <rsmarco@live.it>
2024-03-13 16:50:35 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+const pageForm = document.getElementById("lISTA DELLA SPESA");
+pageForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const nameInputField = document.getElementById("principale");
+  console.log(nameInputField.value);
+  const userCard = document.createElement("div");
+  userCard.innerHTML = `
+    <p> ${nameInputField.value}</p>`;
+  nameInputField.value = "";
+  userCard.addEventListener("click", function () {
+    this.classList.toggle("tagliato");
+  });
+  const button = document.createElement("button");
+  button.innerHTML = "Elimina";
+
+  button.addEventListener("click", function (e) {
+    console.log(e.target);
+    e.target.parentElement.remove();
+  });
+
+  userCard.appendChild(button);
+
+  const listaDeiDesideri = document.getElementById("listadeidesideri");
+  listaDeiDesideri.appendChild(userCard);
+});
